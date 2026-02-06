4cee95eadff8bd0e7be12232f345c4b2778a5e5c
dennismutahi650-alt <dennismutahi650@gmail.com>
2026-02-06 16:31:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,6 @@ noBtn.addEventListener("mouseover", () => {
 // 2. What happens when they click "Yes"
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Yay! See you on the 14th! 😘😘 I Love you Njeri😘";
-    mainGif.src = "";
+    mainGif.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmVqYzczdnA1NDBrcG8waXdnenFwMTdoczZsZGU4Nno4OTBzYW53NiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/IpeXBG7uNM6XJKUVcg/giphy.webp";
     noBtn.style.display = "none"; // Hide the No button
 });
